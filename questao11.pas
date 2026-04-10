35852b7821dd96b62db1a85995d9acadf4018bb5
Davi Matos <davimatis577@gmail.com>
2026-04-10 09:16:04 -0300

    Ultimo commit

--- questao11.pas
+++ questao11.pas
@@ -1,7 +1,19 @@
-﻿Program
-    onze;
+﻿Program onze;
 var
-    num:integer;
+    salario:real;
 begin
+    write('Informe o seu salário: ');
+    read(salario);
+    if (salario >= 0) and (salario <=600) then
+        write('Isento do desconto INSS')
+    else if (salario > 600) and (salario <= 1200) then
+        write('O desconto do INSS é de 20% do seu salário.')
+    else if(salario > 1200) and (salario<=2000) then
+        write('O desconto do INSS é de 25% do seu salário.')
+    else if salario > 2000 then
+        write('O desconto do INSS é de 30% do seu salário.')
+    else
+        write('Valor inválido');
+end.
     
     
---
+++ questao12.pas
@@ -0,0 +1,24 @@
+﻿program doze;
+var
+nome : string;
+nota1, nota2, media: real;
+
+begin
+write('Digite o nome do aluno: ');
+read(nome);
+write('Digite a primeira nota: ');
+read(nota1);
+write('Digite a segunda nota: ');
+read(nota2);
+
+media := (nota1+nota2)/2;
+if media < 0 then
+    write('Notas inválidas!')
+else if media >=7 then
+    write('Aprovado! Sua média foi: ', media:0:2)
+else if (media < 7) and (media >= 3) then
+    write('Prova final! Sua média foi: ', media:0:2)
+else
+    write('Reprovado! Sua média foi: ', media:0:2);
+
+end.
